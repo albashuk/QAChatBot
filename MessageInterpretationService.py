daf5ef2d2624209c3f4f0ab6571b918087df8c48
albashuk <al.bashuk@gmail.com>
2023-05-09 14:05:32 +0300

    Small improvements

--- MessageInterpretationService.py
+++ MessageInterpretationService.py
@@ -1,3 +1,5 @@
+import math
+
 from BertWrapper import BertWrapper
 from Dictionary import Dictionary
 
@@ -19,10 +21,12 @@ class MessageInterpretationService:
                    interp1: MessageInterpretation,
                    interp2: MessageInterpretation,
                    bert_multiplier: float) -> float:
-        bert_cos = self.__cos(interp1.bert_vec, interp2.bert_vec)
-        dict_cos = self.__cos(interp1.dict_vec, interp2.dict_vec)
-
-        return (bert_multiplier * bert_cos + (1 - bert_multiplier) * dict_cos).item()
+        bert_cos = self.__bertCosSim(interp1.bert_vec, interp2.bert_vec)
+        if interp1.dict_vec is None and interp2.dict_vec is None:
+            return bert_cos
+        else:
+            dict_cos = self.__dictCosSim(interp1.dict_vec, interp2.dict_vec)
+            return (bert_multiplier * bert_cos + (1 - bert_multiplier) * dict_cos).item()
 
     def toInterpretation(self, message: str, dictionary: Dictionary) -> MessageInterpretation:
         bert_vec = self.__bert([message])[1]
@@ -33,13 +37,22 @@ class MessageInterpretationService:
     def __dictionaryUse(self, msg: str, dictionary: Dictionary):
         words = self.__parseMsgOnWords(msg)
         if dictionary.size() == 0:
-            dict_vec = [1.0]
+            dict_vec = None
         else:
-            dict_vec = [0.0] * dictionary.size()
+            dict_vec = []
             for word in words:
-                if dictionary.index(word) is not None:
-                    dict_vec[dictionary.index(word)] += 1
-        return torch.tensor([dict_vec]).to(self.__device)
+                index = dictionary.index(word)
+                if index is not None:
+                    dict_vec.append(index)
+        return dict_vec
+
+    def __bertCosSim(self, vec1, vec2):
+        return self.__cos(vec1, vec2)
+
+    def __dictCosSim(self, dict1, dict2):
+        if len(dict1) == 0 or len(dict2) == 0:
+            return 0.5 if len(dict1) == 0 and len(dict2) == 0 else 0.0
+        return len(set(dict1).intersection(dict2)) / math.sqrt(len(dict) * len(dict2))
 
     @staticmethod
     def __parseMsgOnWords(msg: str):
